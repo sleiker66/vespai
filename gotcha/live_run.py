9e109bf0556fe2b4c150fa39fd72374daa8d510b
sleiker66 <jannick.dv@web.de>
2026-06-15 13:23:49 +0200

    Added --max-fps command line argument

--- gotcha/live_run.py
+++ gotcha/live_run.py
@@ -1,20 +1,28 @@
+import argparse
+
 import cv2
 
 from sensors import HornetSensor
+
+parser = argparse.ArgumentParser(description=("Runs a live demo of VespAI "
+                                              "using the default camera."))
+parser.add_argument("-mf", "--max-fps", type=int, default=30)
+args = parser.parse_args()
+if args.max_fps < 0:
+    raise RuntimeError("Cannot use --max-fps negative.")
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     raise RuntimeError("Cannot open Camera.")
 
 sensor = HornetSensor()
-MAX_FPS = 30
 ret, frame = cap.read()
 while ret is not None:
     detection = sensor.detect(frame)
     if detection is not None:
         frame = detection.result
-    cv2.imshow('Detection', frame)
-    cv2.waitKey(1_000 // MAX_FPS)
+    cv2.imshow("Detection", frame)
+    cv2.waitKey(1_000 // args.max_fps)
     ret, frame = cap.read()
 
 print("No frame returned, exiting...")
