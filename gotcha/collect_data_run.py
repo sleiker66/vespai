b5e4cf820a5b8090e068238a3a0964964cf3c77e
sleiker66 <jannick.dv@web.de>
2026-06-26 12:54:23 +0200

    script for collection image data with motion detection

---
+++ gotcha/collect_data_run.py
@@ -0,0 +1,38 @@
+import argparse
+import datetime as dt
+import pathlib
+import time
+
+import cv2
+
+import sensors
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--brake", type=int, default=3,
+                    help="Brake after motion was detected in seconds.")
+parser.add_argument("-sd", "--save-dir", type=str, required=True)
+parser.add_argument("-a", "--min-motion-area", type=int, default=1.3e4,
+                    help="Pixel area in which motion is discounted.")
+parser.add_argument("-p", "--print", action="store_true")
+args = parser.parse_args()
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    raise RuntimeError("Cannot open Camera.")
+
+sensor = sensors.HornetSensor()
+ret, frame = cap.read()
+while ret is not None:
+    motion = sensor.detect_motion(frame)
+    if motion:
+        if args.print:
+            print("Motion detected")
+        file_name = pathlib.Path(args.save_dir, f"{dt.datetime.now()}.jpg")
+        cv2.imwrite(file_name, frame)
+        time.sleep(args.brake)
+    ret, frame = cap.read()
+
+print("No frame returned, exiting...")
+
+
+
