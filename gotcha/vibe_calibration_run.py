23e81d8e63fcbc803b39339266fadeb8fb230738
sleiker66 <jannick.dv@web.de>
2026-06-26 14:06:56 +0200

    made video file arg not required

--- gotcha/vibe_calibration_run.py
+++ gotcha/vibe_calibration_run.py
@@ -7,7 +7,7 @@ import sensors
 parser = argparse.ArgumentParser()
 parser.add_argument("--area-tol", type=int, default=1.3e4)
 parser.add_argument("--max-samples", type=int, default=200)
-parser.add_argument("--video-file", type=str, required=True)
+parser.add_argument("--video-file", type=str)
 args = parser.parse_args()
 
 cap = cv2.VideoCapture(args.video if args.video else 0)
