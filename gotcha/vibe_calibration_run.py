c344fec7a56a862c2fe4de356896d62ae92cc15d
sleiker66 <jannick.dv@web.de>
2026-06-26 14:01:05 +0200

    vibe calibration

---
+++ gotcha/vibe_calibration_run.py
@@ -0,0 +1,15 @@
+import argparse
+
+import cv2
+
+import sensors
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--area-tol", type=int, default=1.3e4)
+parser.add_argument("--max-samples", type=int, default=200)
+parser.add_argument("--video-file", type=str, required=True)
+args = parser.parse_args()
+
+cap = cv2.VideoCapture(args.video if args.video else 0)
+sensor = sensors.HornetSensor()
+sensor.test_vibe_calibration(args.area_tol, cap, args.max_samples)
