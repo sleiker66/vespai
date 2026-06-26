18b0cad8e9270c5f53e109a1eeb6c6071f58c0a4
sleiker66 <jannick.dv@web.de>
2026-06-26 14:12:24 +0200

    fixed argument

--- gotcha/vibe_calibration_run.py
+++ gotcha/vibe_calibration_run.py
@@ -10,6 +10,6 @@ parser.add_argument("--max-samples", type=int, default=200)
 parser.add_argument("--video-file", type=str)
 args = parser.parse_args()
 
-cap = cv2.VideoCapture(args.video if args.video else 0)
+cap = cv2.VideoCapture(args.video_file if args.video_file else 0)
 sensor = sensors.HornetSensor()
 sensor.test_vibe_calibration(args.area_tol, cap, args.max_samples)
